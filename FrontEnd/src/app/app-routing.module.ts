08476d589a53b7f521b31c0e3a93a801165844bd
Jacob Ratima <jacobratima@gmail.com>
2024-08-07 09:54:28 +1200

    Added Transactions component

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'transactions', component: TransactionsComponent },
 ];
 
 export const appRoutingProviders = [
