bd62cc72bf05ce7639e0d21ae5e9e49f1da7d14f
Jacob Ratima <jacobratima@gmail.com>
2024-08-07 08:12:35 +1200

    Login/Register Functionality Implemented

---
+++ FrontEnd/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  supabaseUrl: 'https://dpakpzztgathoeynksvu.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwYWtwenp0Z2F0aG9leW5rc3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ1ODA5ODksImV4cCI6MjAxMDE1Njk4OX0.49Q0kOhXgZdZkkTEJrIQAYLbyvhq0Z1xEsdHWKs3h74',
+};
